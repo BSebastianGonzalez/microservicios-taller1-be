5068ddd5118841c7433a389f1fefbe8295f235d1
BSebastianGonzalez <bsgonzalez14a@gmail.com>
2025-08-27 20:00:56 -0500

    feat: adding Categoria  model, repository, service, dto and controller

--- src/main/java/com/taller1/taller_1/dto/CategoriaDTO.java
+++ src/main/java/com/taller1/taller_1/dto/CategoriaDTO.java
@@ -4,13 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class DenunciaDTO {
+public class CategoriaDTO {
     private String titulo;
     private String descripcion;
-    private List<Long> categoriasIds;
 }
--- src/main/java/com/taller1/taller_1/model/Categoria.java
+++ src/main/java/com/taller1/taller_1/model/Categoria.java
@@ -4,12 +4,14 @@ package com.taller1.taller_1.model;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Categoria {
