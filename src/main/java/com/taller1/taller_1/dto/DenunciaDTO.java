e74a2590eea7c859b809f61dfe6b0d5306b9360d
BSebastianGonzalez <bsgonzalez14a@gmail.com>
2025-08-27 19:46:45 -0500

    feat: adding Denuncia model, repository, service, dto and controller

---
+++ src/main/java/com/taller1/taller_1/dto/DenunciaDTO.java
@@ -0,0 +1,9 @@
+package com.taller1.taller_1.dto;
+
+import java.util.List;
+
+public class DenunciaDTO {
+    public String titulo;
+    public String descripcion;
+    public List<Long> categoriasIds;
+}
---
+++ src/main/java/com/taller1/taller_1/repository/DenunciaRepository.java
@@ -0,0 +1,7 @@
+package com.taller1.taller_1.repository;
+
+import com.taller1.taller_1.model.Denuncia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DenunciaRepository extends JpaRepository<Denuncia,Long> {
+}
